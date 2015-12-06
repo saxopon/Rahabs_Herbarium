ad49e62cacc15de1c061e38d0d8e72a6726ba453
Elektra Wrenholt <ewrenholt@gmail.com>
2015-12-06 12:07:19 -0600

    Display changes

--- RahabiOS/ViewController.m
+++ RahabiOS/ViewController.m
@@ -9,10 +9,11 @@
 @interface ViewController() {
 }
 
-@property NSMutableArray *soundsArray;
+@property NSMutableDictionary *soundsDictionary;
 @property (strong) IBOutlet UILabel *Display;
 @property bool playingmusic;
-@property (strong) NSTimer *timer;
+@property (strong) NSTimer *clipTimer;
+@property (strong) NSTimer *displayTimer;
 
 @end
 
@@ -20,7 +21,7 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	_soundsArray = [NSMutableArray new];
+	_soundsDictionary = [NSMutableDictionary new];
 }
 
 -(void)timer:(NSTimer*)t
@@ -34,21 +35,36 @@
 -(void)updateButtonTitleStart {
 	[self.Button setTitle:@"Start" forState:UIControlStateNormal];
 }
+-(void)updateDisplay {
+	NSMutableString *display = [[NSMutableString alloc] init];
+	for (NSString *key in _soundsDictionary) {
+		AVAudioPlayer *thisPlayer = [_soundsDictionary objectForKey:key];
+		[display appendString:key];
+		[display appendFormat:@" %.01f", [thisPlayer duration] - [thisPlayer currentTime]];
+		[display appendString:@"\n"];
+	}
+	[_Display setText:display];
+}
 
 - (IBAction)buttonClick:(id)sender {
 	
 	if (_playingmusic == true) {
 		_playingmusic = false;
-		for (AVAudioPlayer *a in _soundsArray) [a stop];
+		for (NSString *key in _soundsDictionary) {
+			[[_soundsDictionary objectForKey:key] stop];
+		}
 		[self updateButtonTitleStart];
-		[_soundsArray removeAllObjects];
-		[_timer invalidate];
+		[_soundsDictionary removeAllObjects];
+		[_clipTimer invalidate];
+		[_displayTimer invalidate];
+		[_Display setText:@""];
 	}
 	else {
 		_playingmusic = true;
 		[self updateButtonTitleStop];
 		[self playRandomClip];
-		self.timer = [NSTimer scheduledTimerWithTimeInterval:14.0 target:self selector:@selector(timer:) userInfo:nil repeats:YES];
+		self.clipTimer = [NSTimer scheduledTimerWithTimeInterval:14.0 target:self selector:@selector(timer:) userInfo:nil repeats:YES];
+		self.displayTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateDisplay) userInfo:nil repeats:YES];
 	}
 }
 
@@ -57,23 +73,26 @@
 	NSURL *soundUrl = [NSURL fileURLWithPath:path];
 	AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
 	[audioPlayer setDelegate:self];
-	[_soundsArray addObject:audioPlayer];
+	NSString *track = [NSString stringWithFormat:@"Clip: %@", title];
+	[_soundsDictionary setValue:audioPlayer forKey:track];
 	[audioPlayer prepareToPlay];
 	[audioPlayer play];
-	[_Display setText:[NSString stringWithFormat:@"Clip: %@ \t %.01f", title, [audioPlayer duration]]];
+	[self updateDisplay];
 }
 
 -(void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
-	[_soundsArray removeObject:player];
+	NSString *deletableKey;
+	for (NSString *key in _soundsDictionary) {
+		if ([_soundsDictionary objectForKey:key] == player)
+			deletableKey = key;
+	}
+	[_soundsDictionary removeObjectForKey:deletableKey];
 }
 
 -(void)playRandomClip
 {
 	int r = arc4random_uniform(49) + 1;
 	[self playClip:[NSString stringWithFormat:@"%d", r]];
-	NSLog(@"Clip %d", r);
-	NSLog(@"%@", _soundsArray);
-
 }
 
 @end
